1dc2f0b5ac98a757b24e4718842daeb3122b440d
fabioApablaza@github.com <fabio_nfl@hotmail.com>
2022-04-26 14:30:23 -0300

    input files added

--- app/lexical_analizer.py
+++ app/lexical_analizer.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 from .automatas import comment_recognizer, white_space_recognizer, identifier_keyword_recognizer, number_recognizer,special_symbol_recognizer,arithmetical_operator_recognizer,relational_operator_recognizer,parenthesis_recognizer
 
-
+code = open("ArchivoDePrueba.txt", "r")
+source_code = code.readline()
+code.close()
 
 def source_code_to_lexems(source_code:str):
-    tokens=[]
-    pending_source_code=source_code+" "
-    while(len(pending_source_code)>0):
-        pending_source_code,tokens=white_space_recognizer(pending_source_code)
-        if(len(pending_source_code)==0): break
+    tokens = []
+    pending_source_code = source_code+" "
+    while(len(pending_source_code) > 0):
+        pending_source_code, tokens = white_space_recognizer(pending_source_code)
+        if(len(pending_source_code) == 0):
+            break
         
         #More automatas
         
