83aa5597a9ef297ab1bb62361a50fd2b9bdc6e4c
fabioApablaza@github.com <fabio_nfl@hotmail.com>
2022-04-26 17:27:16 -0300

    funtion call added

--- app/lexical_analizer.py
+++ app/lexical_analizer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 from .automatas import comment_recognizer, white_space_recognizer, identifier_keyword_recognizer, number_recognizer,special_symbol_recognizer,arithmetical_operator_recognizer,relational_operator_recognizer,parenthesis_recognizer
 
+
 code = open("ArchivoDePrueba.txt", "r")
 source_code = code.readline()
 code.close()
+
 
 def source_code_to_lexems(source_code:str):
     tokens = []
@@ -11,9 +13,17 @@ def source_code_to_lexems(source_code:str):
     while(len(pending_source_code) > 0):
         pending_source_code, tokens = white_space_recognizer(pending_source_code)
         if(len(pending_source_code) == 0):
-            break
-        
-        #More automatas
-        
-        pass
-    pass
+            return tokens
+        pending_source_code, tokens = identifier_keyword_recognizer(pending_source_code, tokens)
+
+        pending_source_code, tokens = special_symbol_recognizer(pending_source_code, tokens)
+
+        pending_source_code, tokens = comment_recognizer(pending_source_code, tokens)
+
+        pending_source_code, tokens = number_recognizer(pending_source_code, tokens)
+
+        pending_source_code, tokens = arithmetical_operator_recognizer(pending_source_code, tokens)
+
+        pending_source_code, tokens = relational_operator_recognizer(pending_source_code, tokens)
+
+        pending_source_code, tokens = parenthesis_recognizer(pending_source_code, tokens)
